5c50b25ad3c7136c968554ba16779b31b3726ad2
Hashinimalindi <hashinihettihewa2002@gmail.com>
2026-06-19 22:02:23 +0530

    create HomePage

---
+++ src/page/HomePage.jsx
@@ -0,0 +1,15 @@
+import HeroGrid from "../components/home/HeroGrid";
+import ServicesSection from "../components/home/ServicesSection";
+import FAQSection from "../components/home/FAQSection";
+
+function HomePage() {
+  return (
+    <>
+      <HeroGrid />
+      <ServicesSection />
+      <FAQSection />
+    </>
+  );
+}
+
+export default HomePage;
