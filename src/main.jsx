253bb2f756c6325e587416bb023b5a0ddfc5f754
sathsararns <sathsararns@gmail.com>
2026-06-28 14:09:12 +0530

    refactor App and main components for improved structure and readability

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 
-import 'leaflet/dist/leaflet.css'
+import App from "./App";
+import { AuthProvider } from "./context/AuthContext";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <BrowserRouter> 
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <AuthProvider>
       <App />
-    </BrowserRouter>
-  </StrictMode>,
-)
+    </AuthProvider>
+  </BrowserRouter>
+);
