9cf395484bbc74bfa77bde1a3d6bd5f01bb25216
sathsararns <sathsararns@gmail.com>
2026-07-08 14:30:00 +0530

    add Toaster component for notifications

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,23 @@
+import { Toaster } from "react-hot-toast";
 import AppRoutes from "./routes/AppRoutes";
 
 export default function App() {
-  return <AppRoutes />;
+  return (
+    <>
+      <Toaster
+        position="top-right"
+        reverseOrder={false}
+        toastOptions={{
+          duration: 3000,
+          style: {
+            borderRadius: "10px",
+            background: "#333",
+            color: "#fff",
+          },
+        }}
+      />
+
+      <AppRoutes />
+    </>
+  );
 }
